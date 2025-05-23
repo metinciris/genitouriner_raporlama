7e6cc053f0ec11aceb405524b0ae5319bb20779f
Metin ÇİRİŞ <metin@metinciris.com.tr>
2025-05-23 17:51:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/genitouriner_raporlama/',
+
   build: {
-    outDir: 'docs',       // ← statik dosyalar docs/ altında
-    emptyOutDir: true     // ← her build’te temizlesin
+    outDir: 'docs',       // → Build çıktısı docs/ altına yazılsın
+    emptyOutDir: true     // → Her build öncesi temizlesin
   }
 })
