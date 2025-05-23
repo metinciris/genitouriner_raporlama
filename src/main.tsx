708cefd0711b8b011852f6c16ea545fa16a31b02
Metin ÇİRİŞ <metin@metinciris.com.tr>
2025-05-23 14:39:13 +0300

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RenalPelvisUreterPathologySystem from './renal-pelvis-ureter-pathology-system';
+
+import './index.css'; // varsa stil
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RenalPelvisUreterPathologySystem />
+  </React.StrictMode>
+);
